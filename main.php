d63f42c2146997c8242ceb3f16a23a8a58bf56c0
Emil Galkin <lovely20161102@gmail.com>
2018-07-14 19:34:35 +0300

    first commit

---
+++ main.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Global Input App
+Plugin URI: http://URI_Of_Global_Input_App
+Description: This plugin is to make your wordpress website support the Global Input App, so you can use your mobile as a sign-in device while you are 	 signing in on your website via a browser on your computer.
+
+Version: GIP 0.0.1
+Author: Emil Galkin
+Author URI: http://emil_galkin.com
+License: GIP001
+*/
+?>
